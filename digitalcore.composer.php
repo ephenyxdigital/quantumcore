cdf01f0ae49abe1736e88e9d046478203fe81fd3
Near EarthObject <55352817+ephenyxshop@users.noreply.github.com>
2025-06-20 16:23:54 +0200

    1.1.7

--- digitalcore.composer.php
+++ digitalcore.composer.php
@@ -7,7 +7,7 @@ if (!defined('DIGITAL_CORE_CLASS_DIR')) {
     define('DIGITAL_CORE_CLASS_DIR', dirname(__FILE__).'/src/classes/');
 }
 if (!defined('DIGITAL_CORE_LIB_DIR')) {
-    define('DIGITAL_CORE_LIB_DIR', '/vendor/phenyxdigital/phenyxcore/lib/');
+    define('DIGITAL_CORE_LIB_DIR', '/vendor/phenyxdigital/quantumcore/lib/');
 }
 if (!defined('DIGITAL_CORE_PHENYXGRID_DIR')) {
     define('DIGITAL_CORE_PHENYXGRID_DIR', DIGITAL_CORE_LIB_DIR.'paramgrid/');
